175d1cbe0d04acb3fb76ae4de96e08d71911c9c2
Jeff Johnston <jeffwjohn@yahoo.com>
2020-09-29 17:47:40 -0500

    pass first test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,13 @@
+// const { template } = require('@babel/core');
+// const { exportAllDeclaration } = require('@babel/types');
 const Manager = require('../lib/Manager');
+
+test('creates a manager object', () => {
+    const manager = new Manager('Jody');
+
+    expect(manager.name).toBe('Jody');
+    expect(manager.id).toEqual(expect.any(Number));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
+
+});
