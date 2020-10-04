4800c5c89a9424e9f9f2d7cf78da2dc171b39665
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-03 20:10:44 -0500

    add README

---
+++ README.md
@@ -0,0 +1,35 @@
+# Team Profile Generator
+![](https://img.shields.io/badge/License-MIT-blue.svg)
+## Description 
+Team Profile Generator is an interactive, Node.js-driven application that captures user input via npm Inquirer. The data is then harnessed to dynamically render development team data to an html page linked to a developer-provided, default stylesheet.
+
+## Table of Contents
+* [Installation](#installation) 
+* [Usage](#usage) 
+* [License](#license) 
+* [Contributing](#contributing)
+* [Tests](#tests)  
+* [Questions](#questions)
+
+ 
+### Installation
+  Clone or fork the code from the README-Generator repository on GitHub. Open the code in a standard code editing application, such as VS Code.
+### Usage
+ Open an intregrated terminal and type the command "node index.js." Follow the prompts to generate your custom team profile page!
+### License
+  MIT
+### Contributing
+Generator is an open source project, and anyone is encourged to contribute by cloning or forking the code and working to improve its function and versatility.
+
+### Tests
+Try skipping questions that say they are required. Try answering both yes and no to the confirmation prompts to ensure the expected outcome is obtained. Try skipping optional questions and make sure their respective categories do not show up in the dynamically created table of contents.
+
+### Questions
+    
+##### Interested in other projects from this developer? Visit the following GitHub profile:
+https://github.com/jeffwjohn
+    
+##### Send any questions to the following email address:
+jeffwjohn@yahoo.com
+    
+    
--- src/page-template.js
+++ src/page-template.js
@@ -67,8 +67,8 @@ const generateMembers = membersArray => {
               Engineer
             </h5>
             <p>ID: ${id}</p>
-              <p>Email: ${email}</p>
-              <a href="https://github.com/${github}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>GitHub</a>
+              <a href="mailto:${email}" class="btn mt-auto">Email: ${email}</a>
+              <a href="https://github.com/${github}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>Visit My GitHub Page</a>
             </div>
           `;
         }
@@ -92,8 +92,7 @@ const generateMembers = membersArray => {
               </h5>
               <p>ID: ${id}</p>
               <p>School: ${school}</p>
-              <p>Email: ${email}</p>
-              <a href="${email}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+              <a href="mailto:${email}" class="btn mt-auto">Email: ${email}</a>
             </div>
           `;
         }
@@ -138,9 +137,7 @@ module.exports = templateData => {
         <div class="container flex-row justify-space-between align-center py-3">
           <h1 class="page-title text-secondary bg-dark py-2 px-3">${manager.name}'s Team</h1>
           <nav class="flex-row">
-            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-              manager.email
-            }">GitHub</a>
+            <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/jeffwjohn/team-profile-generator">GitHub</a>
           </nav>
         </div>
       </header>
@@ -153,9 +150,7 @@ module.exports = templateData => {
       </h5>
       <p>ID: ${manager.id}</p> 
       <p>Office: ${manager.officeNumber}</p>
-      <p>Email: ${manager.email}</p>   
-      <a href="${manager.email}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-    </div>
+      <a href="mailto:${manager.email}" class="btn mt-auto">Email: ${manager.email}</a>  
             ${generateMembers(members)}
       </main>
       <footer class="container text-center py-3">
